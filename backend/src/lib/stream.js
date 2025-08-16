247e0ec38c87c3f2e0970440dfcb845c7cc2404b
nikhil-kamboj420 <kambojnikhil44@gmail.com>
2025-08-16 22:25:02 +0530

    setup stream for upserting user

---
+++ backend/src/lib/stream.js
@@ -0,0 +1,23 @@
+import { StreamChat } from "stream-chat";
+import "dotenv/config";
+
+const apiKey = process.env.STREAM_API_KEY;
+const apiSecret = process.env.STREAM_API_SECRET;
+
+if (!apiKey || !apiSecret) {
+  console.error(
+    "Please set STREAM_API_KEY and STREAM_API_SECRET environment variables"
+  );
+}
+const streamClient = StreamChat.getInstance(apiKey, apiSecret);
+
+export const upsertStreamUser = async (userData) => {
+  try {
+    await streamClient.upsertUsers([userData]);
+    return userData;
+  } catch (error) {
+    console.log("Error upserting Stream user:", error.message);
+  }
+};
+
+export const generateStreamToken = async (userId) => {};
