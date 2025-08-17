a7334a902f8c676f0eaff9b5f00a34ce9a551101
nikhil-kamboj420 <kambojnikhil44@gmail.com>
2025-08-17 22:45:07 +0530

    add onboarding route

--- backend/src/routes/authRoute.js
+++ backend/src/routes/authRoute.js
@@ -1,9 +1,10 @@
 import express from "express";
 import * as authControllers from "../controllers/authControllers.js";
+import { protectRoute } from "../middleware/authMiddleware.js";
 const router = express.Router();
 
 router.route("/signup").post(authControllers.handleSignup);
 router.route("/login").post(authControllers.handleLogin);
 router.route("/logout").post(authControllers.handleLogout);
-
+router.route("/onboarding").post(protectRoute, authControllers.handleOnboard);
 export default router;
