a371560c624d4c2806b155e9b5774a9101ca4c30
nikhil-kamboj420 <kambojnikhil44@gmail.com>
2025-08-17 22:48:18 +0530

    add middlewares and import connectDB

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,11 +1,16 @@
-import "dotenv/config";   
+import "dotenv/config";
 import express from "express";
-
+import cookieParser from "cookie-parser";
+import authRoute from "./routes/authRoute.js";
+import { connectDB } from "./lib/db_conn.js";
 const app = express();
 const PORT = process.env.SERVER_PORT || 5000;
-import authRoute from "./routes/authRoute.js"
+
+app.use(express.json());
+app.use(cookieParser());
 app.use("/api/auth", authRoute);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
+  connectDB();
 });
