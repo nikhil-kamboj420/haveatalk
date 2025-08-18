2635944793680f9a87666588caef06279754ab57
nikhil-kamboj420 <kambojnikhil44@gmail.com>
2025-08-18 20:07:27 +0530

    add /me route

--- backend/src/routes/authRoute.js
+++ backend/src/routes/authRoute.js
@@ -7,4 +7,8 @@ router.route("/signup").post(authControllers.handleSignup);
 router.route("/login").post(authControllers.handleLogin);
 router.route("/logout").post(authControllers.handleLogout);
 router.route("/onboarding").post(protectRoute, authControllers.handleOnboard);
+// check if user is logged in
+router.get("/me", protectRoute, (req, res) => {
+  res.status(200).json({ success: true, user: req.user });
+});
 export default router;
