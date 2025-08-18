0bb6879df278239aad10afb344b4e4e4cb19fb80
nikhil-kamboj420 <kambojnikhil44@gmail.com>
2025-08-18 20:01:17 +0530

    add userRoute

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,7 @@ import "dotenv/config";
 import express from "express";
 import cookieParser from "cookie-parser";
 import authRoute from "./routes/authRoute.js";
+import userRoute from "./routes/userRoute.js";
 import { connectDB } from "./lib/db_conn.js";
 const app = express();
 const PORT = process.env.SERVER_PORT || 5000;
@@ -9,6 +10,7 @@ const PORT = process.env.SERVER_PORT || 5000;
 app.use(express.json());
 app.use(cookieParser());
 app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
