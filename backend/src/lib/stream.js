2b3cca5807a5d4f11b512427e647c8fa7364d8a9
nikhil-kamboj420 <kambojnikhil44@gmail.com>
2025-08-20 19:08:06 +0530

    define generateStreamToken()

--- backend/src/lib/stream.js
+++ backend/src/lib/stream.js
@@ -20,4 +20,11 @@ export const upsertStreamUser = async (userData) => {
   }
 };
 
-export const generateStreamToken = async (userId) => {};
+export const generateStreamToken = async (userId) => {
+  try {
+    const userIdStr = userId.toString();
+    return streamClient.createToken(userIdStr)
+  } catch (error) {
+    console.error("Error generating Stream token : ", error)
+  }
+};
