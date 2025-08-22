bc757e97c557b5ece1dfdd16bd6f013682ac5bdc
nikhil-kamboj420 <kambojnikhil44@gmail.com>
2025-08-22 14:42:02 +0530

    add cors

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,6 @@
 import "dotenv/config";
 import express from "express";
+import cors from "cors";
 import cookieParser from "cookie-parser";
 import authRoutes from "./routes/authRoute.js";
 import userRoutes from "./routes/userRoute.js";
@@ -7,7 +8,10 @@ import chatRoutes from "./routes/chatRoute.js";
 import { connectDB } from "./lib/db_conn.js";
 const app = express();
 const PORT = process.env.SERVER_PORT || 5000;
-
+app.use(cors({
+  origin : "http://localhost:5173",
+  credentials : true
+}))
 app.use(express.json());
 app.use(cookieParser());
 app.use("/api/auth", authRoutes);
