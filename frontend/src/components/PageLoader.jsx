49c3cce3e7c5db306a3362a4e9d9890d94e258f0
nikhil-kamboj420 <kambojnikhil44@gmail.com>
2025-08-23 23:50:38 +0530

    add PageLoader

---
+++ frontend/src/components/PageLoader.jsx
@@ -0,0 +1,9 @@
+const PageLoader = () => {
+  return (
+    <div className="flex items-center justify-center h-screen  bg-black">
+      <div className="animate-spin rounded-full h-16 w-16 border-t-4 border-e-white border-opacity-75"></div>
+    </div>
+  );
+};
+
+export default PageLoader;
