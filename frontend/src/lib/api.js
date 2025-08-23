06426123997d19530497b2559f0df965ffafa4fe
nikhil-kamboj420 <kambojnikhil44@gmail.com>
2025-08-23 23:57:26 +0530

    define getAuthUser and completeOnboarding

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -4,3 +4,12 @@ export const signup = async(signupData)=>{
     const res = await axiosInstance.post('/auth/signup', signupData);
     return res.data;
 }
+
+export  const getAuthUser = async () => {
+      const res = await axiosInstance.get("/auth/me");
+      return res.data;
+    }
+export  const completeOnboarding = async () => {
+      const res = await axiosInstance.post("/auth/onboarding");
+      return res.data;
+    }
