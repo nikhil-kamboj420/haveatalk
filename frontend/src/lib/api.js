cc0819f6e8ac93640452d2295f6d2a043b4dc94c
nikhil-kamboj420 <kambojnikhil44@gmail.com>
2025-08-25 20:25:07 +0530

    modify getAuthUser fun()

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -1,15 +1,29 @@
 import { axiosInstance } from "./axios";
 
-export const signup = async(signupData)=>{
-    const res = await axiosInstance.post('/auth/signup', signupData);
-    return res.data;
-}
+export const signup = async (signupData) => {
+  const res = await axiosInstance.post("/auth/signup", signupData);
+  return res.data;
+};
+export const login = async (loginData) => {
+  const res = await axiosInstance.post("/auth/login", loginData);
+  return res.data;
+};
+export const logout = async () => {
+  const res = await axiosInstance.post("/auth/logout");
+  return res.data;
+};
 
-export  const getAuthUser = async () => {
-      const res = await axiosInstance.get("/auth/me");
-      return res.data;
-    }
-export  const completeOnboarding = async () => {
-      const res = await axiosInstance.post("/auth/onboarding");
-      return res.data;
-    }
+export const getAuthUser = async () => {
+  try {
+    const res = await axiosInstance.get("/auth/me");
+    return res.data;
+  } catch (err) {
+    console.log("Error in getAuthUser :", err);
+    return null;
+  }
+};
+
+export const completeOnboarding = async (formState) => {
+  const res = await axiosInstance.post("/auth/onboarding", formState);
+  return res.data;
+};
