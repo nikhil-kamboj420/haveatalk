285187b3183fb63c2d998c15f98d49fa4070ef32
nikhil-kamboj420 <kambojnikhil44@gmail.com>
2025-08-26 23:59:40 +0530

    add data-theme

--- frontend/src/components/PageLoader.jsx
+++ frontend/src/components/PageLoader.jsx
@@ -1,6 +1,9 @@
+import { useThemeStore } from "../store/useThemeStore";
+
 const PageLoader = () => {
+  const { theme} = useThemeStore();
   return (
-    <div className="flex items-center justify-center h-screen  bg-black">
+    <div className="flex items-center justify-center h-screen  bg-black" data-theme={theme}>
       <div className="animate-spin rounded-full h-16 w-16 border-t-4 border-e-white border-opacity-75"></div>
     </div>
   );
