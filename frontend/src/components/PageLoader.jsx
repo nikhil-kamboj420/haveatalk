dd1eb2095773be5a726a9f2f61ab105faeefcc4b
nikhil-kamboj420 <kambojnikhil44@gmail.com>
2025-08-27 22:32:28 +0530

    add min-h-[1rem] min-w-[1rem]

--- frontend/src/components/PageLoader.jsx
+++ frontend/src/components/PageLoader.jsx
@@ -3,7 +3,7 @@ import { useThemeStore } from "../store/useThemeStore";
 const PageLoader = () => {
   const { theme} = useThemeStore();
   return (
-    <div className="flex items-center justify-center h-screen  bg-black" data-theme={theme}>
+    <div className="flex items-center justify-center min-h-[1rem] min-w-[1rem]" data-theme={theme}>
       <div className="animate-spin rounded-full h-16 w-16 border-t-4 border-e-white border-opacity-75"></div>
     </div>
   );
