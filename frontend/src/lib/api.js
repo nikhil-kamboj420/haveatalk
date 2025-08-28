fd1549300d372a86ea84cd8b02ed56cd473b213f
nikhil-kamboj420 <kambojnikhil44@gmail.com>
2025-08-28 18:43:37 +0530

    add fun()s for user queries and mutation

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -27,3 +27,19 @@ export const completeOnboarding = async (formState) => {
   const res = await axiosInstance.post("/auth/onboarding", formState);
   return res.data;
 };
+export const getUserFriends = async () => {
+  const res = await axiosInstance.get("/users/friends");
+  return res.data;
+};
+export const getRecommendedUsers = async () => {
+  const res = await axiosInstance.get("/users");
+  return res.data;
+};
+export const getOutgoingFriendReqs = async () => {
+  const res = await axiosInstance.get("/users/outgoing-friend-requests");
+  return res.data;
+};
+export const sendFriendReqs = async (userId) => {
+  const res = await axiosInstance.post(`/users/friend-request/${userId}`);
+  return res.data;
+};
