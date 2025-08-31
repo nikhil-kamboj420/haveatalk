290d396e7108f0c0aeb9d54cca46e65c1550f9cb
nikhil-kamboj420 <kambojnikhil44@gmail.com>
2025-08-31 16:30:29 +0530

    create NonotificationFound

---
+++ frontend/src/components/NonotificationFound.jsx
@@ -0,0 +1,15 @@
+
+const NonotificationFound = () => {
+  return (
+        <div className="max-w-screen min-h-screen flex flex-col justify-center items-center">
+            <span>🔔</span>
+            <h2>No notification Yet!</h2>
+            <p>When you receive friend requests or messages, they'll appear</p>
+            <span>here.</span>
+        </div>
+  )
+}
+
+export default NonotificationFound;
+
+
