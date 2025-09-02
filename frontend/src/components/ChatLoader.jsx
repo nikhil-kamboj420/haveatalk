408d3e2e75849e1a07915008342933321343b768
nikhil-kamboj420 <kambojnikhil44@gmail.com>
2025-09-02 23:31:14 +0530

    add chatLoader

---
+++ frontend/src/components/ChatLoader.jsx
@@ -0,0 +1,9 @@
+export const ChatLoader = () => {
+  return (
+        <div className=" max-w-screen h-[93vh] grid place-content-center    text-7xl ">
+      <p>
+        <span className="spinner"></span> {"       "}Connecting to Chat ...
+      </p>
+    </div>
+  );
+};
