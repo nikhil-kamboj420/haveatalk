e6ef08ce5291dd57e18a352a15ed1095233a5568
nikhil-kamboj420 <kambojnikhil44@gmail.com>
2025-09-09 23:57:23 +0530

    add tailwind css

--- frontend/src/pages/FriendsPage.jsx
+++ frontend/src/pages/FriendsPage.jsx
@@ -11,48 +11,48 @@ import { ReccommendedFriends } from "../components/ReccommendedFriends";
 import toast from "react-hot-toast";
 
 const FriendsPage = () => {
-    const queryClient = useQueryClient();
-const [outgoingReqsIds, setOutgoingReqsIds] = useState(new Set());
-let { data: friends = [], isLoding: loadFriends } = useQuery({
-  queryKey: ["friends"],
-  queryFn: getUserFriends,
-});
-const { data: getRecUsers = [], isLoding: loadRecUsers } = useQuery({
-  queryKey: ["getRecUsers"],
-  queryFn: getRecommendedUsers,
-});
-const { data: getOutgoingFrdReqs } = useQuery({
-  queryKey: ["getOutgoingFrdReqs"],
-  queryFn: getOutgoingFriendReqs,
-});
+  const queryClient = useQueryClient();
+  const [outgoingReqsIds, setOutgoingReqsIds] = useState(new Set());
+  let { data: friends = [], isLoding: loadFriends } = useQuery({
+    queryKey: ["friends"],
+    queryFn: getUserFriends,
+  });
+  const { data: getRecUsers = [], isLoding: loadRecUsers } = useQuery({
+    queryKey: ["getRecUsers"],
+    queryFn: getRecommendedUsers,
+  });
+  const { data: getOutgoingFrdReqs } = useQuery({
+    queryKey: ["getOutgoingFrdReqs"],
+    queryFn: getOutgoingFriendReqs,
+  });
 
-const { mutate: sendFrdReqsMutation, isPending } = useMutation({
-  mutationFn: sendFriendReqs,
-  onSuccess: ({ message }) => {
-    queryClient.invalidateQueries({ queryKey: ["getOutgoingFrdReqs"] });
-    toast.success(message);
-  },
-  onError: (err) => {
-    toast.error(err?.response?.data?.message);
-  },
-});
+  const { mutate: sendFrdReqsMutation, isPending } = useMutation({
+    mutationFn: sendFriendReqs,
+    onSuccess: ({ message }) => {
+      queryClient.invalidateQueries({ queryKey: ["getOutgoingFrdReqs"] });
+      toast.success(message);
+    },
+    onError: (err) => {
+      toast.error(err?.response?.data?.message);
+    },
+  });
 
-useEffect(() => {
-  const outgoingIds = new Set();
-  if (getOutgoingFrdReqs && getOutgoingFrdReqs.length > 0) {
-    getOutgoingFrdReqs.forEach((req) => {
-      outgoingIds.add(req.recipient._id);
-    });
-    setOutgoingReqsIds(outgoingIds);
-  }
-}, [getOutgoingFrdReqs]);
+  useEffect(() => {
+    const outgoingIds = new Set();
+    if (getOutgoingFrdReqs && getOutgoingFrdReqs.length > 0) {
+      getOutgoingFrdReqs.forEach((req) => {
+        outgoingIds.add(req.recipient._id);
+      });
+      setOutgoingReqsIds(outgoingIds);
+    }
+  }, [getOutgoingFrdReqs]);
 
   return (
     <>
       {" "}
       <title>Friends | HaveaTalk</title>
-      <link rel="icon" href="https://flagcdn.com/24x18/in.png" />
-      <div className="home-page min-h-[100vh]">
+      <link rel="icon" href="/friends-fav-icon.png" />
+      <div className="home-page min-h-screen  max-w-screen bg-[linear-gradient(130deg,rgb(14,9,24)_0%,#6742bc_50%,rgb(14,9,24)_100%)] m-auto ">
         <Friends loadFriends={loadFriends} friends={friends} />
         <ReccommendedFriends
           loadRecUsers={loadRecUsers}
