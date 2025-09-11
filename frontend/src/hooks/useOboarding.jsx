30c80a3940c6da88ce7a75fbffce2480dc45d2ca
nikhil-kamboj420 <kambojnikhil44@gmail.com>
2025-09-12 00:03:02 +0530

    add setTimeout

--- frontend/src/hooks/useOboarding.jsx
+++ frontend/src/hooks/useOboarding.jsx
@@ -1,14 +1,19 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import toast from "react-hot-toast";
 import { completeOnboarding } from "../lib/api";
+import { useNavigate } from "react-router";
 
 const useOboarding = () => {
+  const navigate = useNavigate();
   const queryClient = useQueryClient();
   const { mutate, isPending } = useMutation({
     mutationFn: completeOnboarding,
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: ["authUser"] });
       toast.success("Profile  OnBoarded successfully !");
+      setTimeout(() => {
+        navigate("/");
+      }, 1500);
     },
     onError: (err) => {
       const data = err.response?.data;
