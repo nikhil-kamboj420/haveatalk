b875df4a597b6722037f9cdbe5c6f08b3deda6c4
nikhil-kamboj420 <kambojnikhil44@gmail.com>
2025-09-11 23:47:06 +0530

    modified HomePage

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,63 +1,24 @@
-import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
-import { useEffect, useState } from "react";
-import {
-  getUserFriends,
-  getRecommendedUsers,
-  getOutgoingFriendReqs,
-  sendFriendReqs,
-} from "../lib/api";
-import { Friends } from "../components/Friends";
-import { ReccommendedFriends } from "../components/ReccommendedFriends";
-import toast from "react-hot-toast";
+import { useState, useEffect } from "react";
+import Hero from "../components/Hero";
+import PageLoader from "../components/PageLoader";
 
 const HomePage = () => {
-  const queryClient = useQueryClient();
-  const [outgoingReqsIds, setOutgoingReqsIds] = useState(new Set());
-  let { data: friends = [], isLoding: loadFriends } = useQuery({
-    queryKey: ["friends"],
-    queryFn: getUserFriends,
-  });
-  const { data: getRecUsers = [], isLoding: loadRecUsers } = useQuery({
-    queryKey: ["getRecUsers"],
-    queryFn: getRecommendedUsers,
-  });
-  const { data: getOutgoingFrdReqs } = useQuery({
-    queryKey: ["getOutgoingFrdReqs"],
-    queryFn: getOutgoingFriendReqs,
-  });
-
-const { mutate: sendFrdReqsMutation, isPending } = useMutation({
-  mutationFn: sendFriendReqs,
-  onSuccess: ({message}) => {
-    queryClient.invalidateQueries({ queryKey: ["getOutgoingFrdReqs"] });
-    toast.success(message);
-  },
-  onError: (err) => {
-    toast.error(err?.response?.data?.message);
-  },
-});
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    const outgoingIds = new Set();
-    if (getOutgoingFrdReqs && getOutgoingFrdReqs.length > 0) {
-      getOutgoingFrdReqs.forEach((req) => {
-        outgoingIds.add(req.recipient._id);
-      });
-      setOutgoingReqsIds(outgoingIds);
-    }
-  }, [getOutgoingFrdReqs]);
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 100);
+
+    return () => clearTimeout(timer);
+  }, []);
 
   return (
-    <div className="home-page min-h-[100vh]">
-      <Friends loadFriends={loadFriends} friends={friends} />
-      <ReccommendedFriends
-        loadRecUsers={loadRecUsers}
-        getRecUsers={getRecUsers}
-        outgoingReqsIds={outgoingReqsIds}
-        sendFrdReqsMutation={sendFrdReqsMutation}
-        isPending={isPending}
-      />
-    </div>
+    <>
+      <title>Home | HaveaTalk</title>
+      <link className="rounded-full" rel="icon" href="/home-fav-icon.png" />
+      {loading ? <PageLoader /> : <Hero />}
+    </>
   );
 };
 
