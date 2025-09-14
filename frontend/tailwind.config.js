33e67e461b08cf4ae8d27f32221b76ff627b856c
nikhil-kamboj420 <kambojnikhil44@gmail.com>
2025-09-14 22:48:56 +0530

    removed kabisat font

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -4,7 +4,6 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        kabisat: ["Kabisat", "serif"],
         organical: ["Organical" || "mori"],
         dragrace: ["Dragrace" || "mori"],
       },
