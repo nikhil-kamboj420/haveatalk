e9d4861b92d87ff8ed45308f65908cf07dfe143f
nikhil-kamboj420 <kambojnikhil44@gmail.com>
2025-09-20 23:54:57 +0530

    added sendUserMessage

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -61,3 +61,7 @@ export async function getStreamToken() {
   const response = await axiosInstance.get(`/chat/token`);
   return response.data;
 }
+export async function sendUserMessage(userMessage) {
+  const response = await axiosInstance.post(`/chat/bot/message`, userMessage);
+  return response.data;
+}
