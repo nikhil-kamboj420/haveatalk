fdf038167be037e30bb478562b22837f8f8e699e
nikhil-kamboj420 <kambojnikhil44@gmail.com>
2025-09-21 00:02:30 +0530

    added route for sending message to chatbot

--- backend/src/routes/chatRoute.js
+++ backend/src/routes/chatRoute.js
@@ -1,8 +1,9 @@
 import express from "express"
 import { protectRoute } from "../middleware/authMiddleware.js";
-import { getStreamToken } from "../controllers/chatContoller.js";
+import { getStreamToken, getUserMessage } from "../controllers/chatContoller.js";
 
 const  router = express.Router();
 
 router.route("/token").get(protectRoute, getStreamToken)
+router.route("/bot/message").post(protectRoute, getUserMessage )
 export default router
